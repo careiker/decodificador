f822be4e07aeae2c50a2e80756e0dee09de16cad
Carlos Reiker <70608412+Carlos-Reiker@users.noreply.github.com>
2023-01-05 17:28:16 -0300

    test 1

---
+++ src/assets/script/script.js
@@ -0,0 +1,26 @@
+/*function encryptVowels(string) {
+    let encryptedString = '';
+  
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === 'a') {
+        encryptedString += '1';
+      } else if (string[i] === 'e') {
+        encryptedString += '2';
+      } else if (string[i] === 'i') {
+        encryptedString += '3';
+      } else if (string[i] === 'o') {
+        encryptedString += '4';
+      } else if (string[i] === 'u') {
+        encryptedString += '5';
+      } else {
+        encryptedString += string[i];
+      }
+    }
+  
+    return encryptedString;
+  }
+  
+  // Exemplo de uso:
+  const originalString = 'hello world';
+  const encryptedString = encryptVowels(originalString);
+  console.log(encryptedString); // 'h2ll4 w4rld'*/
