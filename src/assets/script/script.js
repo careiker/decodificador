7b0c7cf27a5d7edf77a30cf65d691cfe32e62799
Carlos Reiker <70608412+Carlos-Reiker@users.noreply.github.com>
2023-01-06 22:28:08 -0300

    test script

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,38 +1,26 @@
-/*function encryptVowels(string) {
-    let encryptedString = '';
-  
-    for (let i = 0; i < string.length; i++) {
-      if (string[i] === 'a') {
-        encryptedString += '1';
-      } else if (string[i] === 'e') {
-        encryptedString += '2';
-      } else if (string[i] === 'i') {
-        encryptedString += '3';
-      } else if (string[i] === 'o') {
-        encryptedString += '4';
-      } else if (string[i] === 'u') {
-        encryptedString += '5';
-      } else {
-        encryptedString += string[i];
-      }
-    }
-  
-    return encryptedString;
-  }
-  
-  // Exemplo de uso:
-  const originalString = 'hello world';
-  const encryptedString = encryptVowels(originalString);
-  console.log(encryptedString); // 'h2ll4 w4rld'*/
 
-function substituirVogais() {
+function criptografarVogais() {
   var texto = document.getElementById("entrada").value;
+  
+  texto = texto.replace(/a/g, "ai");
+  texto = texto.replace(/e/g, "enter");
+  texto = texto.replace(/i/g, "imes");
+  texto = texto.replace(/o/g, "ober");
+  texto = texto.replace(/u/g, "ufat");
+  
 
-  texto = texto.replace(/a/gi, "1");
-  texto = texto.replace(/e/gi, "2");
-  texto = texto.replace(/i/gi, "3");
-  texto = texto.replace(/o/gi, "4");
-  texto = texto.replace(/u/gi, "5");
+  document.getElementById("mensagem").value = texto;
+}
+
+
+function descriptografarVogais() {
+  var texto = document.getElementById("entrada").value;
+  
+  texto = texto.replace(/ai/g, "a");
+  texto = texto.replace(/enter/g, "e");
+  texto = texto.replace(/imes/g, "i");
+  texto = texto.replace(/ober/g, "o");
+  texto = texto.replace(/ufat/g, "u");
 
   document.getElementById("mensagem").value = texto;
 }
