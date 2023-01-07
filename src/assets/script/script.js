ea3c9f15d63065b2f6820352658f1d955fb057f8
Carlos Reiker <70608412+Carlos-Reiker@users.noreply.github.com>
2023-01-07 20:43:52 -0300

    Update script.js

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,36 +1,8 @@
-/*function criptografarVogais() {
-  // Obtém o texto do elemento <textarea>
-  var texto = document.getElementById('entrada').value;
-
-  // Criptografa o texto
-  texto = texto.replace(/a/g, 'ai');
-  texto = texto.replace(/e/g, 'enter');
-  texto = texto.replace(/i/g, 'imes');
-  texto = texto.replace(/o/g, 'ober');
-  texto = texto.replace(/u/g, 'ufat');
-
-  // Exibe o texto criptografado no elemento <textarea>
-  document.getElementById('mensagem').value = texto;
-}
-
-function descriptografarVogais() {
-  // Obtém o texto do elemento <textarea>
-  var texto = document.getElementById('entrada').value;
-
-  // Descriptografa o texto
-  texto = texto.replace(/ai/g, 'a');
-  texto = texto.replace(/enter/g, 'e');
-  texto = texto.replace(/imes/g, 'i');
-  texto = texto.replace(/ober/g, 'o');
-  texto = texto.replace(/ufat/g, 'u');
-
-  // Exibe o texto descriptografado no elemento <textarea>
-  document.getElementById('mensagem').value = texto;
-}*/
 
 const inputTexto = document.querySelector(".input");
 const mensagem = document.querySelector(".output");
 
+/*FUNÇÃO PARA CRIPTOGRAFAR*/
   function btnCrypt() {
     const textoEncriptado = encriptar(inputTexto.value)
     mensagem.value = textoEncriptado
@@ -50,6 +22,7 @@ const mensagem = document.querySelector(".output");
     return stringEncriptada;
   }
 
+  /*FUNÇÃO PARA DESCRIPTOGRAFAR*/
   function btnDescrypt() {
     const textoDescriptado = descriptar(inputTexto.value)
     mensagem.value = textoDescriptado
