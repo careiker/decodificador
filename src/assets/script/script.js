b93d6bbf498302a72f7b4e1bac0de333d2bc8569
Carlos Reiker <70608412+Carlos-Reiker@users.noreply.github.com>
2023-01-07 00:30:30 -0300

    test

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,26 +1,30 @@
-
 /*function criptografarVogais() {
-  var texto = document.getElementById("entrada").value;
-  
-  texto = texto.replace(/a/g, "ai");
-  texto = texto.replace(/e/g, "enter");
-  texto = texto.replace(/i/g, "imes");
-  texto = texto.replace(/o/g, "ober");
-  texto = texto.replace(/u/g, "ufat");
-  
+  // Obtém o texto do elemento <textarea>
+  var texto = document.getElementById('entrada').value;
 
-  document.getElementById("mensagem").value = texto;
+  // Criptografa o texto
+  texto = texto.replace(/a/g, 'ai');
+  texto = texto.replace(/e/g, 'enter');
+  texto = texto.replace(/i/g, 'imes');
+  texto = texto.replace(/o/g, 'ober');
+  texto = texto.replace(/u/g, 'ufat');
+
+  // Exibe o texto criptografado no elemento <textarea>
+  document.getElementById('mensagem').value = texto;
 }
 
-
 function descriptografarVogais() {
-  var texto = document.getElementById("entrada").value;
-  
-  texto = texto.replace(/ai/g, "a");
-  texto = texto.replace(/enter/g, "e");
-  texto = texto.replace(/imes/g, "i");
-  texto = texto.replace(/ober/g, "o");
-  texto = texto.replace(/ufat/g, "u");
+  // Obtém o texto do elemento <textarea>
+  var texto = document.getElementById('entrada').value;
 
-  document.getElementById("mensagem").value = texto;
+  // Descriptografa o texto
+  texto = texto.replace(/ai/g, 'a');
+  texto = texto.replace(/enter/g, 'e');
+  texto = texto.replace(/imes/g, 'i');
+  texto = texto.replace(/ober/g, 'o');
+  texto = texto.replace(/ufat/g, 'u');
+
+  // Exibe o texto descriptografado no elemento <textarea>
+  document.getElementById('mensagem').value = texto;
 }*/
+
